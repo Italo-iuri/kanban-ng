a5d65b9475115737b5a393f7ee17258315463dc8
Italo Iuri <106288345+Italo-iuri@users.noreply.github.com>
2022-10-27 22:50:11 -0300

    Kanban done day 3

--- kanban1/src/app/pages/main-view/main-view.component.ts
+++ kanban1/src/app/pages/main-view/main-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -10,6 +13,43 @@ export class MainViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  board: Board = new Board ('Test board', [
+    new Column('Ideas', [
+      "Some Ideas",
+      "More Ideas",
+      "Just one more"
+    ]),
+    new Column ('Research', [
+      "A research",
+      "Another research",
+      "One more research"
+    ]),
+    new Column ('Todo', [
+      "New project",
+      "New Kanban",
+      "Update Portfolio"
+    ]),
+    new Column ('Done', [
+      "First Kanban",
+      "Copying more than doing",
+    ])
+
+  ]);
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
